6670f6bd74cc63896d21a63dbfa27db9434fd718
Harshi Bansal <harshibansal.92@gmail.com>
2023-01-02 19:56:18 -0800

    Webpage sample

--- app.py
+++ app.py
@@ -1,3 +1,20 @@
-import flask
+from flask import Flask, flash, redirect, render_template
 
-print("hello, world")
+
+# Configure application
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    return render_template("index.html")
